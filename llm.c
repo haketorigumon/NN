2308a8d8a89b3b6ec105049d941c9860e24d7099
haketorigumon <haketorigumon@gmail.com>
2026-04-15 08:29:51 +0800

    Update llm.c

--- llm.c
+++ llm.c
@@ -166,9 +166,9 @@ static void forward(int token, float *tm_outputs) {
             int c = 0;
             for (int i = 0; i < CLAUSE_FEATURES; i++) {
                 if (clause->pattern[i] = 0) {
-                    if (features[i] = 0) a += 1;
+                    if (features[i] == 0) a += 1;
                 } else {
-                    if (features[i] = 1) b += 1;
+                    if (features[i] == 1) b += 1;
                 }
             }
             if (a >= b) {
