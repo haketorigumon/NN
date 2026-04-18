5e8d95c621587022b81fafc3dc9f1548cfaeb7ac
haketorigumon <haketorigumon@gmail.com>
2026-04-18 09:01:29 +0800

    Update tmlm.c

--- tmlm.c
+++ tmlm.c
@@ -254,15 +254,45 @@ static float train(uint8_t token, uint8_t next_token) {
             if (!GET_BIT(clauses_of_all_class, f + next_token * CLAUSES_PER_CLASS)) {
                 for (int i = 0; i < META_CLAUSES; i++) {
                     if (GET_BIT(pattern_2, (next_token * CLAUSES_PER_CLASS + f) * META_CLAUSES + i)) {
-                        if (!GET_BIT(meta_clause_outputs, META_CLAUSES + i)) {
+                        if (!GET_BIT(meta_clause_outputs, i)) {
                             if (p >= (float)rand() / RAND_MAX) {
                                 TOGGLE_BIT(pattern_2, (next_token * CLAUSES_PER_CLASS + f) * META_CLAUSES + i);
                             } else {
                                 for (int k = 0; k < VOCAB_SIZE; k++) {
-                                    if (GET_BIT(clause_outputs, k)) {
+                                    if (GET_BIT(clause_outputs, i * VOCAB_SIZE + k)) {
                                         if (!GET_BIT(features, k)) {
                                             for (int h = 0; h < META_CLAUSES; h++) {
-                                                if (GET_BIT(pattern, h))
+                                                if (GET_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h)) {
+                                                    if (GET_BIT(meta_clause_outputs, h)) {
+                                                        if (p >= (float)rand() / RAND_MAX) {
+                                                            TOGGLE_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h);
+                                                        }
+                                                    }
+                                                } else {
+                                                    if (!GET_BIT(meta_clause_outputs, h)) {
+                                                        if (p >= (float)rand() / RAND_MAX) {
+                                                            TOGGLE_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        if (GET_BIT(features, k)) {
+                                            for (int h = 0; h < META_CLAUSES; h++) {
+                                                if (GET_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h)) {
+                                                    if (!GET_BIT(meta_clause_outputs, h)) {
+                                                        if (p >= (float)rand() / RAND_MAX) {
+                                                            TOGGLE_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h);
+                                                        }
+                                                    }
+                                                } else {
+                                                    if (GET_BIT(meta_clause_outputs, h)) {
+                                                        if (p >= (float)rand() / RAND_MAX) {
+                                                            TOGGLE_BIT(pattern, (i * VOCAB_SIZE + k) * META_CLAUSES + h);
+                                                        }
+                                                    }
+                                                }
                                             }
                                         }
                                     }
@@ -270,7 +300,7 @@ static float train(uint8_t token, uint8_t next_token) {
                             }
                         }
                     } else {
-                        if (GET_BIT(meta_clause_outputs, META_CLAUSES + i)) {
+                        if (GET_BIT(meta_clause_outputs, i)) {
                             if (p >= (float)rand() / RAND_MAX) {
                                 TOGGLE_BIT(pattern_2, (next_token * CLAUSES_PER_CLASS + f) * META_CLAUSES + i);
                             }
