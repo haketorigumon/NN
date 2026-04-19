20d66366b86bceff20de6547f61f2c0e591a90ea
haketorigumon <haketorigumon@gmail.com>
2026-04-19 16:02:32 +0800

    Update tmlm.c

--- tmlm.c
+++ tmlm.c
@@ -252,9 +252,19 @@ static float train(uint8_t token, uint8_t next_token) {
                                                         TOGGLE_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + h);
                                                     }
                                                 }
-                                                if (GET_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + CLAUSES_OF_META_LAYER * FEATURES_PER_CLAUSE_OF_BLOCK + h) == GET_BIT(mem, h)) {
+                                                if (GET_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + CLAUSES_OF_META_LAYER * FEATURES_PER_CLAUSE_OF_BLOCK + h) != GET_BIT(mem, h)) {
                                                     if (p >= (float)rand() / RAND_MAX) {
                                                         TOGGLE_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + CLAUSES_OF_META_LAYER * FEATURES_PER_CLAUSE_OF_BLOCK + h);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        if (!GET_BIT(features, k)) {
+                                            for (int h = 0; h < FEATURES_PER_CLAUSE_OF_BLOCK; h++) {
+                                                if (GET_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + h) == GET_BIT(mem, h)) {
+                                                    if (p >= (float)rand() / RAND_MAX) {
+                                                        TOGGLE_BIT(pattern, (i * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + CLAUSES_OF_INPUT_LAYER * FEATURES_PER_CLAUSE_OF_INPUT_LAYER + k) * FEATURES_PER_CLAUSE_OF_BLOCK + h);
                                                     }
                                                 }
                                             }
