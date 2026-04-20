2df059b080a02b0e85864e86eb07c90d1799cfe6
haketorigumon <haketorigumon@gmail.com>
2026-04-20 11:43:27 +0800

    Update tmlm.c

--- tmlm.c
+++ tmlm.c
@@ -143,13 +143,14 @@ static int forward(uint8_t token) {
     uint8_t meta_layer_output[(CLAUSES_OF_META_LAYER + 7) / 8];
 
     clauses(meta_layer_output, mem, pattern, CLAUSES_OF_META_LAYER, FEATURES_PER_CLAUSE_OF_BLOCK);
+    clauses(mem, features, meta_layer_output, CLAUSES_OF_INPUT_LAYER, FEATURES_PER_CLAUSE_OF_INPUT_LAYER);
+
     uint8_t *class_layer_outputs = (uint8_t *)malloc((CLAUSES_OF_CLASS_LAYER + 7) / 8);
     if (class_layer_outputs == NULL) {
         perror("malloc failed in train");
         exit(1);
     }
     clauses_2(class_layer_outputs, mem, pattern_2, CLAUSES_OF_CLASS_LAYER, FEATURES_PER_CLAUSE_OF_CLASS);
-    clauses(mem, features, meta_layer_output, CLAUSES_OF_INPUT_LAYER, FEATURES_PER_CLAUSE_OF_INPUT_LAYER);
 
     int logits[CLASSES] = {0};
     for (int k = 0; k < CLASSES; k++) {
@@ -179,9 +180,11 @@ static int forward(uint8_t token) {
 static float train(uint8_t token, uint8_t next_token) {
     srand((float)(time(NULL) ^ clock()));
     uint8_t *features = &token;
-    uint8_t meta_layer_output[(CLAUSES_OF_META_LAYER + 7) / 8] = {0};
+    uint8_t meta_layer_output[(CLAUSES_OF_META_LAYER + 7) / 8];
 
     clauses(meta_layer_output, mem, pattern, CLAUSES_OF_META_LAYER, FEATURES_PER_CLAUSE_OF_BLOCK);
+    clauses(mem, features, meta_layer_output, CLAUSES_OF_INPUT_LAYER, FEATURES_PER_CLAUSE_OF_INPUT_LAYER);
+
     uint8_t *class_layer_outputs = (uint8_t *)malloc((CLAUSES_OF_CLASS_LAYER + 7) / 8);
     if (class_layer_outputs == NULL) {
         perror("malloc failed in train");
@@ -189,8 +192,6 @@ static float train(uint8_t token, uint8_t next_token) {
     }
     clauses_2(class_layer_outputs, mem, pattern_2, CLAUSES_OF_CLASS_LAYER, FEATURES_PER_CLAUSE_OF_CLASS);
 
-    clauses(mem, features, meta_layer_output, CLAUSES_OF_INPUT_LAYER, FEATURES_PER_CLAUSE_OF_INPUT_LAYER);
-    
     int logits[CLASSES] = {0};
     
     for (int k = 0; k < CLASSES; k++) {
