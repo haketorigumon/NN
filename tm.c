f611515d4d88258b49416f31b81c8e47478ba9d3
haketorigumon <haketorigumon@gmail.com>
2026-04-23 14:27:27 +0800

    Update tm.c

--- tm.c
+++ tm.c
@@ -32,16 +32,12 @@
 #define FEATURES_PER_CLAUSE_OF_INPUT_LAYER INPUT_DIM
 #define FEATURES_OF_INPUT_LAYER FEATURES_PER_CLAUSE_OF_INPUT_LAYER * CLAUSES_OF_INPUT_LAYER
 
-#define BLOCKS_OF_OUT_LAYER 256
-#define CLAUSES_PER_OUT_BLOCK 256
-#define FEATURES_PER_CLAUSE_OF_OUT MEM
-#define CLAUSES_OF_OUT_LAYER CLAUSES_PER_OUT_BLOCK * BLOCKS_OF_OUT_LAYER
-#define FEATURES_OF_OUT_LAYER CLAUSES_OF_OUT_LAYER * FEATURES_PER_CLAUSE_OF_OUT
-
 
 #define CLASSES VOCAB_SIZE
-#define FEATURES_PER_CLASS CLAUSES_PER_OUT_BLOCK
-#define FEATURES_OF_CLASS_LAYER FEATURES_PER_CLASS * CLASSES
+#define CLAUSES_PER_CLASS 256
+#define CLAUSES_OF_CLASS_LAYER CLASSES * CLAUSES_PER_CLASS
+#define FEATURES_PER_CLAUSE_OF_CLASS MEM
+
 
 #define SET_BIT(arr, n)     ((arr)[(n)/8] |=  (1U << ((n)%8)))
 #define CLEAR_BIT(arr, n)   ((arr)[(n)/8] &= ~(1U << ((n)%8)))
@@ -51,7 +47,6 @@
 uint8_t pattern[(FEATURES_OF_MEM_LAYER * 2 + 7) / 8];
 uint8_t pattern_2[(FEATURES_PER_META_BLOCK * 2 + 7) / 8];
 uint8_t pattern_3[(FEATURES_OF_OUT_LAYER * 2 + 7) / 8];                               
-uint8_t pattern_4[(FEATURES_PER_CLASS + 7) / 8];                               
 
 uint8_t mem[(MEM + 7) / 8];
 
@@ -64,7 +59,6 @@ static void save_weights(const char *filename) {
     fwrite(pattern,   1, sizeof(pattern),   fp);
     fwrite(pattern_2, 1, sizeof(pattern_2), fp);
     fwrite(pattern_3, 1, sizeof(pattern_3), fp);
-    fwrite(pattern_4, 1, sizeof(pattern_4), fp);
     fclose(fp);
     printf("权重已保存到文件：%s\n", filename);
 }
